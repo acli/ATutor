cb3081ea41a0def422ebf61a05a0206350e7a740
Ambrose Li <ambrose.li@gmail.com>
2013-05-21 16:46:39 -0400

    Checking this in for the moment so that some bug fix can be checked in as its own thing

--- mods/forums_new/edit_post.php
+++ mods/forums_new/edit_post.php
@@ -16,6 +16,7 @@ define('AT_MODULE_ROOT', './');
 require(AT_MODULE_ROOT.'lib/module.inc.php');
 define('AT_INCLUDE_PATH', at_include_path_from(AT_MODULE_ROOT));
 require (AT_INCLUDE_PATH.'vitals.inc.php');
+$forums_d = MODULE_DIR;
 
 require(AT_MODULE_ROOT.'lib/forums.inc.php');
 

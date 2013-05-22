23376fc7d2e29eb10a957b4d8f924270c4ef6fcb
Ambrose Li <ambrose.li@gmail.com>
2013-05-22 19:49:33 -0400

    Make it work at the new, temporary location

--- mods/forums_new/sublinks.php
+++ mods/forums_new/sublinks.php
@@ -1,7 +1,8 @@
 <?php
 
 if (!defined('AT_INCLUDE_PATH')) { exit; }
-require(AT_INCLUDE_PATH.'../mods/_standard/forums/lib/forums.inc.php');
+require(AT_INCLUDE_PATH.'../'.AT_FORUMS_NEW__DIR.'/lib/module.inc.php');
+require(AT_INCLUDE_PATH.'../'.AT_FORUMS_NEW__DIR.'/lib/forums.inc.php');
 
 global $db;
 
@@ -20,7 +21,7 @@ foreach ($all_forums as $shared => $forums) {
             $cnt++;
 
             $link_title = AT_print($row['title'], 'forums.title').' ('.AT_DATE('%F %j, %g:%i',$row['last_post'],AT_DATE_MYSQL_DATETIME).')';
-            $list[] = '<a href="'.url_rewrite('mods/_standard/forums/forum/index.php?fid='.$row['forum_id'], AT_PRETTY_URL_IS_HEADER).'"'.
+            $list[] = '<a href="'.url_rewrite(AT_FORUMS_NEW__DIR.'/forum/index.php?fid='.$row['forum_id'], AT_PRETTY_URL_IS_HEADER).'"'.
                       (strlen($link_title) > SUBLINK_TEXT_LEN ? ' title="'.$link_title.'"' : '') .'>'. 
                       validate_length($link_title, SUBLINK_TEXT_LEN, VALIDATE_LENGTH_FOR_DISPLAY) .'</a>';
         }
